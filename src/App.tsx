98d55dad10ec3896c1718dbd05e405dd71ed92e2
w3bh4ck <lucky@softcom.ng>
2021-03-05 04:09:02 +0100

    Tooling: Updated linting and precommit hooks. https://trello.com/c/LFyrFfI3/37-tooling-add-pre-commit-hook-with-lint-staged-using-the-stipulated-eslint-prettier-rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  NavLink,
-  Switch,
-} from "react-router-dom";
-
-import Overview from "pages/Overview";
+import './App.css';
+import Overview from './pages/Overview';
 
 function App() {
   return (
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,7 @@
-import React, { FC } from "react";
-import { Col, Grid, Layout as AntDLayout, Row } from "antd";
-import Header from "./Header";
-
-import { layoutPadding } from "utils/responsive";
+import React, { FC } from 'react';
+import { Grid, Layout as AntDLayout, Row } from 'antd';
+import Header from './Header';
+import { layoutPadding } from 'utils/responsive';
 
 const { useBreakpoint } = Grid;
 
@@ -10,13 +9,12 @@ const { Footer, Content } = AntDLayout;
 
 const Layout: FC = ({ children }) => {
   const screens = useBreakpoint();
-  console.log(screens);
 
   return (
     <>
       <AntDLayout>
         <Header />
-        <Content style={{ padding: "50px " + layoutPadding(screens) }}>
+        <Content style={{ padding: '50px ' + layoutPadding(screens) }}>
           <Row gutter={[20, 30]}>{children}</Row>
         </Content>
         <Footer style={{ paddingLeft: layoutPadding(screens) }}>Footer</Footer>
