5d23480525a925db51f6e1d4667d91987648c68a
Tomi Jaga <jagatomi12@gmail.com>
2021-07-06 17:21:03 +0100

    Testnet Integration

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,6 +1,6 @@
 import {PayloadAction} from '@reduxjs/toolkit';
 
-export const setObjectsReducer = <Type>() => (state: Type, {payload}: PayloadAction<Type>) => {
+export const setObjectsReducer = <Type>() => (state: any, {payload}: PayloadAction<Type>) => {
   Object.keys(state).forEach((key: string) => {
     state[key] = payload[key];
   });
