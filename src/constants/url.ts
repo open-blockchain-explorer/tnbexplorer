6d5854c01f5609df7c41c5fa033a938fd9c786c6
Nikhil Taneja <taneja.nikhil03@gmail.com>
2021-09-28 13:38:46 +0530

    Update url.ts

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,6 +1,6 @@
 export const CORS_BRIDGE = 'https://cors.bridged.cc';
 
-export const BANK_URL = 'http://13.233.77.254';
+export const BANK_URL = 'http://54.183.16.194';
 export const PV_URL = 'http://54.219.234.129';
 
 export const TESTNET_BANK_URL = 'http://bank.tnbexplorer.com';
